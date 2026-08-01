4d21b153b550b0eeb583c604e282c127f3b085d2
ab13830415-dot <ab13830415@gmail.com>
2026-08-01 19:54:19 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("سلام 👋\nربات با موفقیت اجرا شد.")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
